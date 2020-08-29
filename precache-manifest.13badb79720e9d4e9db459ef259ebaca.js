fdcaf7a9ad63683f341fabc9e7b0a6ff68fdc7f2
golibjon94 <golib94jon@gmail.com>
2020-08-29 19:48:32 +0500

    Updates

---
+++ precache-manifest.13badb79720e9d4e9db459ef259ebaca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb4a18e9530c665e3a3ef6b5580d051",
+    "url": "/my-react-app/home/index.html"
+  },
+  {
+    "revision": "1a23c88c5868f972657c",
+    "url": "/my-react-app/home/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b44294ddc48d4e14221a",
+    "url": "/my-react-app/home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a23c88c5868f972657c",
+    "url": "/my-react-app/home/static/js/2.92be2089.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-react-app/home/static/js/2.92be2089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44294ddc48d4e14221a",
+    "url": "/my-react-app/home/static/js/main.88793859.chunk.js"
+  },
+  {
+    "revision": "eec331a4ade80b6197ce",
+    "url": "/my-react-app/home/static/js/runtime-main.8c3a38e2.js"
+  },
+  {
+    "revision": "fb2eef7d06704e1ffd70cf02f2e22410",
+    "url": "/my-react-app/home/static/media/aurora1.fb2eef7d.jpg"
+  },
+  {
+    "revision": "ceb80e720898bcb594c7b9e6a9ad32aa",
+    "url": "/my-react-app/home/static/media/aurora2.ceb80e72.jpg"
+  },
+  {
+    "revision": "9987b65a4aec9e7412b362ec691d581e",
+    "url": "/my-react-app/home/static/media/aurora3.9987b65a.jpg"
+  },
+  {
+    "revision": "07691a57b33c655013199d000a610ae5",
+    "url": "/my-react-app/home/static/media/aurora4.07691a57.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
